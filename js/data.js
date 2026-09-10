a7dbbb6eca7df349822776fd020042a1ab215024
mvnu6 <mbengoemmanuel26@gmail.com>
2026-09-10 14:37:59 +0200

    feat: ajoute de data.js

---
+++ js/data.js
@@ -0,0 +1,10 @@
+export const PAYS_DATA = [
+  { code: 'FR', nom: 'France', capitale: 'Paris', region: 'Europe', population: 67000000 },
+  { code: 'JP', nom: 'Japon', capitale: 'Tokyo', region: 'Asie', population: 125000000 },
+  { code: 'BR', nom: 'Brésil', capitale: 'Brasília', region: 'Amériques', population: 214000000 },
+  { code: 'EG', nom: 'Égypte', capitale: 'Le Caire', region: 'Afrique', population: 109000000 },
+  { code: 'CA', nom: 'Canada', capitale: 'Ottawa', region: 'Amériques', population: 38000000 },
+  { code: 'DE', nom: 'Allemagne', capitale: 'Berlin', region: 'Europe', population: 83000000 },
+  { code: 'AUS', nom: 'Australie', capitale: 'Canberra', region: 'Océanie', population: 25000000 },
+  { code: 'IN', nom: 'Inde', capitale: 'New Delhi', region: 'Asie', population: 1400000000 }
+];
